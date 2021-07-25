fa379672908592b5a8a1738d800390f5f02f5666
Furkan TAN <furkan.tan@icloud.com>
2021-07-25 11:26:05 -0400

    async function configuration

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const Driver = require("./models/Driver");
-const Passenger = require("./models/Passenger");
 const {
   printUpcomingTrips,
   printCurrentTrip,
@@ -9,76 +7,6 @@ const passengerDatabase = require("./database/passenger-database");
 const driverDatabase = require("./database/driver-database");
 const tripDatabase = require("./database/trip-database");
 //const db = require("./test/db");
-
-const driver = new Driver(
-  undefined,
-  "John",
-  "Smith",
-  "john@gmail.com",
-  "123456789",
-  undefined,
-  "123123123"
-);
-
-driver.addVehicle("Car", "Toyota", "Camry", "2021");
-
-const camry = driver.vehicles.find((item) => item.model === "Camry");
-
-const tripDate = new Date("August 17, 2021 08:00:00");
-
-driver.createTrip(camry, tripDate, "New York", "Miami", "100");
-
-printUpcomingTrips(driver);
-
-const trip = driver.upcomingTrips[0];
-
-const passenger = new Passenger(
-  undefined,
-  "George",
-  "Odd",
-  "george@gmail.com",
-  "123456789"
-);
-const passenger2 = new Passenger(
-  undefined,
-  "Fred",
-  "Odd",
-  "george@gmail.com",
-  "123456789"
-);
-const passenger3 = new Passenger(
-  undefined,
-  "Mikael",
-  "Odd",
-  "george@gmail.com",
-  "123456789"
-);
-const passenger4 = new Passenger(
-  undefined,
-  "Dani",
-  "Odd",
-  "george@gmail.com",
-  "123456789"
-);
-const passenger5 = new Passenger(
-  undefined,
-  "Dana",
-  "Odd",
-  "george@gmail.com",
-  "123456789"
-);
-
-passenger.addCard("George", 1234123412341234, 12, 2022, 111, "Credit_Card");
-
-passenger.bookTrip(trip);
-passenger2.bookTrip(trip);
-passenger3.bookTrip(trip);
-passenger4.bookTrip(trip);
-passenger4.bookTrip(trip);
-passenger5.bookTrip(trip);
-printUpcomingTrips(driver);
-
-console.log(trip);
 
 passengerDatabase.save([
   passenger,
